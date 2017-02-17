997b58acdb13c42ce382403dde03bc2f353fdbca
Tina Murimi <Tina Murimi>
2017-02-17 04:23:12 +0300

    Formatiing tweets to ensure emails are recognised

--- fetching_user_tweets.py
+++ fetching_user_tweets.py
@@ -5,6 +5,7 @@ import string
 import datetime
 import re
 import operator 
+import os
 
 from collections import Counter
 from colorama import *
@@ -14,7 +15,7 @@ from twython import Twython, TwythonError
 from termcolor import cprint
 from tqdm import tqdm
 
-import stopWords_module
+from create_stop_words_list import stop_words
 
 
 APP_KEY = '911OxmuGGaf2hIqEde5GHWdtt'
@@ -24,9 +25,9 @@ OAUTH_TOKEN_SECRET = 'gPdGUbNJDWFULl0TjImEMkZuvoceGaE01TmHsBH158JZt'
 
 twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
 
-def user_handle(handle = ''):
-	handle = input("\nEnter user's Twitter handle: ")
-	return handle
+def user_handle(username = ''):
+	username = input("\nEnter user's Twitter handle: ")
+	return username
 
 
 def get_tweets(username = '', source = 'function_input'):
@@ -46,19 +47,27 @@ def get_tweets(username = '', source = 'function_input'):
 
 	else:
 		try:
-			user_TL= twitter.get_user_timeline(screen_name = username, count = 50, include_rts = False) # include_retweets
-			
+			user_TL= twitter.get_user_timeline(screen_name = username, count = 1) # a dict inside a list
+
+			#tweet id for the last tweet
+			last_tweet_id = [dict(user_TL[0])['id']]
+
+			#specify the max_id as the last item in the list (the last tweet id from each extraction).
+			user_TL= twitter.get_user_timeline(screen_name = username, count = 50, include_rts = False, max_id=last_tweet_id[-1])
+
+			# 10 secone rest between api calls
+			#time.sleep(10)
 
 			userTL_tweets = []
 
 			#Save the tweets in a JSON file
 			for i in tqdm(range(0, len(user_TL)), desc = 'Fetching tweets', unit = 'Tweets'):
 				time.sleep(.05)
-				with open('userTweets.json', 'w') as outfile:
-					for tweet in user_TL:
-						userTL_tweets.append(re.sub(r'[^\x00-\x7F]+', '', tweet['text'])) # Remove unicode text
+				for tweet in user_TL:
+					userTL_tweets.append(re.sub(r'[^\x00-\x7F]+', '', tweet['text'])) # Remove unicode text
 
-					json.dump(userTL_tweets, outfile)
+			with open('userTweets.json', 'w') as outfile:
+				json.dump(userTL_tweets, outfile)
 
 			tweets_toReturn = {'tweets':tuple(userTL_tweets), 'username':username}
 			tweets_toReturn = dict(tweets_toReturn)
@@ -75,17 +84,18 @@ def get_tweets(username = '', source = 'function_input'):
 
 
 
-def filtered_Posts(status = '', source = 'function_input'):
+def filtered_posts(status = '', source = 'function_input'):
 
 	""" Function to filter input
 	arg source: 1. function_input - from get_tweets() or user_input
 	"""
-	forbidden_words = stopWords_module.stopWords_list_func()
+	all_stop_words = stop_words()
 
-	tokens_re = forbidden_words['tokens_re']
-	emoticon_re = forbidden_words['emoticon_re']
-	stop_words = forbidden_words['stopWords']
-	punct =  forbidden_words['punct']
+	tokens_re = all_stop_words['tokens_re']
+	emoticon_re = all_stop_words['emoticon_re']
+	bad_words = all_stop_words['stop_words']
+	punct = all_stop_words['punct']
+	email_regex = all_stop_words['email_regex']
 
 	if source == 'user_input':
 		status = status['tweets']
@@ -95,32 +105,44 @@ def filtered_Posts(status = '', source = 'function_input'):
 		status = status['tweets'] #<class 'tuple'>
 
 	if status == '':
-		terms_stop = []
-		return (terms_stop)
+		filtered_tweets = []
+		return filtered_tweets
 
 	else:
 		filtered_tweets = []
 
-		for i in range(len(status)):
+		join_tweets = status
+		join_tweets = ''.join(join_tweets)
 
-			allWords = tokens_re.findall(status[i])
-			allWords = [token if emoticon_re.search(token) else token.lower() for token in allWords]
+		emails_in_tweet = email_regex.findall(join_tweets)
+		emails = []
+		if len(emails_in_tweet) > 0:
 
-			filtered_tweets = filtered_tweets + allWords
+			for e in range(len(emails_in_tweet)):
+				emails.append(emails_in_tweet[e][0].lower())
+
+			for e in emails:
+				join_tweets = re.sub(e, '', join_tweets.lower())
+				join_tweets = re.sub(r'\s', ' ', join_tweets)
+
+		allWords = tokens_re.findall(join_tweets)
+		allWords = [token if emoticon_re.search(token) else token.lower() for token in allWords]
 		
 		# Remove stop words. See stop_words_list.txt	
-		terms_stop = [text for text in filtered_tweets if text.lower() not in stop_words]
+		filtered_tweets = [text for text in allWords if text.lower() not in bad_words]
 
 		# Remove any punctuation marks
-		terms_stop = [text for text in terms_stop if text.lower() not in punct]
+		filtered_tweets = [text for text in filtered_tweets if text.lower() not in punct]
 
-		return terms_stop
+		filtered_tweets = filtered_tweets + emails
+
+		return filtered_tweets
 
 def word_counter(list_of_words = '', source = 'function_input'):
 
 	input_source = source
 
-	list_of_words = filtered_Posts(status = '', source = input_source)
+	list_of_words = filtered_posts(status = '', source = input_source)
 
 	if len(list_of_words) != 0:
 		word_freq = Counter(list_of_words)
